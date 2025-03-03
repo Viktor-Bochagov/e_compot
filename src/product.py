b3346b271533b7bc382132f2a8f76f42056ca5d6
Viktor Bochagov <vitekbochagov@mail.ru>
2025-03-03 17:45:10 +0500

    fixed product.py

--- src/product.py
+++ src/product.py
@@ -11,5 +11,40 @@ class Product:
 
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, dict_of_product, list_of_products: list):
+        """Метод для создания новых объектов класса Product. На вход необходимо подать словарь с параметрами товаров:
+        name, description, price, quantity, а также текущий список товаров (для избежания дублирования позиций)"""
+
+        for product in list_of_products:
+            if dict_of_product['name'] == product.name:
+                product.quantity += dict_of_product['quantity']
+                if product.price < dict_of_product['price']:
+                    product.price = dict_of_product['price']
+                return product
+        return cls(dict_of_product['name'], dict_of_product['description'], dict_of_product['price'],
+                   dict_of_product['quantity'])
+
+    @property
+    def price(self):
+        """Геттер для аттрибута price"""
+
+        return self.__price
+
+    @price.setter
+    def price(self, new_price: int):
+        """Сеттер для изменения аттрибута price"""
+
+        if new_price <= 0:
+            print("Цена не должна быть отрицательной или ровна нулю")
+            return
+        if new_price < self.__price:
+            print("Вы собираетесь уменьшить цену на товар. Для подтверждения нажмите Y \n")
+            confirmation = input()
+            if confirmation.lower() != 'y':
+                print("Цена не изменилась")
+                return
+            self.__price = new_price
