b6241a699d271edc72896e4df740fe80c4b970cb
Viktor Bochagov <vitekbochagov@mail.ru>
2025-03-03 18:23:27 +0500

    Тесты по модулям categoty, product

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,5 +19,32 @@ def second_category():
 
 
 @pytest.fixture
+def third_category():
+    return Category("some category", "category with something", [
+        Product("everything", "everything everywhere and at once", 69.77, 13),
+        Product("nothing", "respectfully accepting donations", 100, 34435353)])
+
+
+@pytest.fixture
 def product():
     return Product("something", "useful tool for testing", 125.50, 666)
+
+
+@pytest.fixture
+def product_in_dict():
+    return {"name": "everything", "description": "everything everywhere and at once", "price": 69.77, "quantity": 13}
+
+
+@pytest.fixture
+def first_list_products():
+    return [Product("anything", "everything you desire", 9999999.99, 1)]
+
+
+@pytest.fixture
+def second_list_products():
+    return [Product("everything", "everything you desire", 9999999.99, 1)]
+
+
+@pytest.fixture
+def third_list_products():
+    return [Product("everything", "everything you desire", 10.50, 1)]
