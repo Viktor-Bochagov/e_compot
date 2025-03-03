f192b206dc66fd0d316d623f086ba1d6e9fbe3c7
Viktor Bochagov <vitekbochagov@mail.ru>
2025-03-03 17:28:38 +0500

    fixed categoty.py

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Класс для категорий товаров"""
 
@@ -12,6 +15,23 @@ class Category:
 
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = list(products)
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, new_product: Product):
+        """Метод для записи новых объектов класса Product в атрибут Category.products"""
+
+        self.__products.append(new_product)
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_str
+
+    @property
+    def products_in_list(self):
+        return self.__products
