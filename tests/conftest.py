144f56c1b1d17d70715cd8411d2a1e367fc9c9d6
Viktor Bochagov <vitekbochagov@mail.ru>
2025-03-12 18:42:55 +0500

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,26 @@
 import pytest
 
 from src.category import Category
+from src.lawngrass_category import LawnGrass
 from src.product import Product
 from src.product_iterator import ProductIterator
+from src.smartphone_category import Smartphone
 
 
 @pytest.fixture
 def product_iphone():
     return Product("iPhone 15 Pro Max", "1Tb", 200000, 20)
 
+
 @pytest.fixture
 def product_huawei():
     return Product("Huawei 7S", "512Gb", 30000, 14)
 
+
 @pytest.fixture
 def category_phone(product_huawei, product_iphone):
     return Category("Смартфоны", "Умные телефоны", [product_iphone, product_huawei])
+
 
 @pytest.fixture
 def new_product_valid_1():
@@ -25,6 +30,7 @@ def new_product_valid_1():
         "price": 180000.0,
         "quantity": 5,
     }
+
 
 @pytest.fixture
 def new_product_valid_2():
@@ -44,3 +50,25 @@ def new_product_valid_3():
 @pytest.fixture
 def product_iterator(category_phone):
     return ProductIterator(category_phone)
+
+
+@pytest.fixture
+def smartphone():
+    return Smartphone(
+        "Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5, 95.5, "S23 Ultra", 256, "Серый"
+    )
+
+
+@pytest.fixture
+def smartphone_2():
+    return Smartphone("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14, 90.3, "Note 11", 1024, "Синий")
+
+
+@pytest.fixture
+def lawngrass():
+    return LawnGrass("Газонная трава", "Выносливая трава", 450.0, 15, "США", "5 дней", "Темно-зеленый")
+
+
+@pytest.fixture
+def lawngrass_2():
+    return LawnGrass("Газонная трава 2", "Элитная трава для газона", 500.0, 20, "Россия", "7 дней", "Зеленый")
