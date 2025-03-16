63031af3e29f6cbf58cf7c0b8f9e536a4d6f03c4
Viktor Bochagov <vitekbochagov@mail.ru>
2025-03-16 13:30:33 +0500

    new_class

---
+++ src/base_product.py
@@ -0,0 +1,10 @@
+from abc import ABC, abstractmethod
+
+
+class BaseProduct(ABC):
+    """Абстрактный класс, содержащий абстрактный метод для добавления новых продуктов"""
+
+    @classmethod
+    @abstractmethod
+    def new_product(cls, *args, **kwargs):
+        pass
