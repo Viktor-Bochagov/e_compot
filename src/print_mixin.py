442e2522d8f27687720bcf5af04c168d9d70f35e
Viktor Bochagov <vitekbochagov@mail.ru>
2025-03-16 13:31:17 +0500

    new_class_mixin

---
+++ src/print_mixin.py
@@ -0,0 +1,13 @@
+class PrintMixin:
+    """Класс миксин, который при создании объекта выводит информацию о нем"""
+
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self):
+        print(repr(self))
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}({self.name}, {self.description}, {self.price}, {self.quantity})"
