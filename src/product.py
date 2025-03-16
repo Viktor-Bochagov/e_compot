2bfad6e0b7a3bba8e5692f82b879a87c614f2249
Viktor Bochagov <vitekbochagov@mail.ru>
2025-03-16 13:31:24 +0500

    update

--- src/product.py
+++ src/product.py
@@ -1,4 +1,8 @@
-class Product:
+from src.base_product import BaseProduct
+from src.print_mixin import PrintMixin
+
+
+class Product(BaseProduct, PrintMixin):
     """Класс для описания товаров, цены и имеющееся в наличии количество"""
 
     name: str
@@ -13,6 +17,7 @@ class Product:
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
 
     def __str__(self):
         """Выводит строковое отображение в заданном формате"""
